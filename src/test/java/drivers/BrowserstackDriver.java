b4f2081c4d41129594800e0f26394a8cb7278356
Ilya Khabarov <khabarov.ilya@gmail.com>
2025-06-08 19:59:18 +0300

    add homework for lesson 20

--- src/test/java/drivers/BrowserstackDriver.java
+++ src/test/java/drivers/BrowserstackDriver.java
@@ -3,54 +3,45 @@ package drivers;
 import com.codeborne.selenide.WebDriverProvider;
 import config.BrowserstackConfig;
 import org.aeonbits.owner.ConfigFactory;
-import org.jspecify.annotations.NonNull;
 import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.MutableCapabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
+import javax.annotation.Nonnull;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.HashMap;
-import java.util.Map;
 
 public class BrowserstackDriver implements WebDriverProvider {
 
     private static final BrowserstackConfig config = ConfigFactory.create(BrowserstackConfig.class);
 
-    @NonNull
+    @Nonnull
     @Override
-    public WebDriver createDriver(@NonNull Capabilities capabilities) {
-        MutableCapabilities mutableCapabilities = new MutableCapabilities();
-        String platform = System.getProperty("platform", "android").toLowerCase();
+    public WebDriver createDriver(@Nonnull Capabilities capabilities) {
+        MutableCapabilities caps = new MutableCapabilities();
 
-        // Общие настройки для BrowserStack (W3C-совместимый формат)
-        Map<String, Object> browserstackOptions = new HashMap<>();
-        browserstackOptions.put("userName", config.user());
-        browserstackOptions.put("accessKey", config.key());
-        browserstackOptions.put("projectName", "First Java Project");
-        browserstackOptions.put("buildName", "browserstack-build-1");
-        browserstackOptions.put("sessionName", "first_test");
+        caps.setCapability("browserstack.user", config.user());
+        caps.setCapability("browserstack.key", config.key());
 
-        // Платформо-зависимые настройки
-        if (platform.equals("ios")) {
-            mutableCapabilities.setCapability("platformName", "iOS");
-            browserstackOptions.put("deviceName", config.iosDevice());
-            browserstackOptions.put("platformVersion", config.iosOsVersion());
-        } else { // Android (по умолчанию)
-            mutableCapabilities.setCapability("platformName", "Android");
-            browserstackOptions.put("deviceName", config.androidDevice());
-            browserstackOptions.put("platformVersion", config.androidOsVersion());
+        String platform = System.getProperty("platform", "android");
+
+        if (platform.equalsIgnoreCase("ios")) {
+            caps.setCapability("device", config.iosDevice());
+            caps.setCapability("os_version", config.iosOsVersion());
+            caps.setCapability("app", config.app());
+        } else {
+            caps.setCapability("device", config.androidDevice());
+            caps.setCapability("os_version", config.androidOsVersion());
+            caps.setCapability("app", config.app());
         }
 
-        // App URL (для BrowserStack)
-        browserstackOptions.put("app", config.app());
-
-        // Упаковываем все в W3C-совместимый формат
-        mutableCapabilities.setCapability("bstack:options", browserstackOptions);
+        caps.setCapability("project", "First Java Project");
+        caps.setCapability("build", "browserstack-build-1");
+        caps.setCapability("name", "first_test");
 
         try {
-            return new RemoteWebDriver(new URL(config.url()), mutableCapabilities);
+            return new RemoteWebDriver(new URL(config.url()), caps);
         } catch (MalformedURLException e) {
             throw new RuntimeException("Invalid Browserstack URL", e);
         }
