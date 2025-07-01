2805ea777faabcc09150002605db5d4728f7c4b7
Ilya Khabarov <khabarov.ilya@gmail.com>
2025-07-01 17:10:20 +0300

    add some changes

--- src/test/java/tests/WikiOnboardingLocalTest.java
+++ src/test/java/tests/WikiOnboardingLocalTest.java
@@ -8,7 +8,7 @@ import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.*;
 import static io.qameta.allure.Allure.step;
 
-@Tag("android_local")
+@Tag("android_browserstack")
 public class WikiOnboardingLocalTest extends TestBase {
 
     @Test
